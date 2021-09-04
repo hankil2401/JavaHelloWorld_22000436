4f622cc9f6c58e132305cae5931a1b53ecd1c4c1
hankil2401 <22000436@handong.edu>
2021-09-04 14:26:57 +0900

    Second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -10,6 +10,18 @@ public class Main {
 		System.out.print("이름 입력 : ");
 		String name = s.next();
 		System.out.println("[" + name + "]님 환영합니다~");
+		
+		System.out.print("Please enter your year of birth (yyyy): ");
+		int birth = s.nextInt();
+		int age = 2021-birth+1;
+		
+		if(age>15) {
+			System.out.println("Welcome to our shop!");
+		}
+		else {
+			System.out.println("That way please, there is area for kids over there!");
+		}
+		
 	}
 
 }
